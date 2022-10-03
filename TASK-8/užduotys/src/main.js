3885f7046835336f92d305c236831f12bbc840d1
deimarut <deimante.rutenaite@gmail.com>
2022-10-03 20:51:58 +0300

    aštunta uzduotis

---
+++ TASK-8/užduotys/src/main.js
@@ -0,0 +1,26 @@
+"use strict";
+//'1. Sukurkite Person klasei savybes "name" ir "surname". Kiekvienai iš jų sukurkite setterius, ir bendrą getterį fullname
+//'2. Sukurkite Person klasei savybę "age". Inkapsuliuokite šią savybę taip, jog reikšmė galėtų būti tik sveiki skaičiai nuo 1 iki 150'
+//'3. Sukurkite Person klasei savybę "height" kurios vertė būtų saugoma centimetrais. Sukurkite šiai savybei setterį, kuris pirmu parametru priimtų reikšmę, o antru parametru priimtų matavimo vienetus: "cm" | "m" | "in". Jeigu antras parametras nėra perduotas, numatytas(default) matavimo vienetas turi būti cm. Getteris turi grąžinti reikšmę centimetrais.
+//'4. Sukurkite Person klasei statinę savybę "heightUnits". Jos tipas turi būti išvardinimas(enum), kurio pasirinkimai yra: "CM", "M", "IN". Numatytoji(default) "heightUnits" reikšmė turi būti centimetrai
+//'5. "height" setterio antram parametrui pakeiskite sąjungos tipą į [4.] užduotyje sukurtą išvardinimą(enum). Priderinkite pavyzdžius ir metodą.
+//'6. "height" geteriui sukurkite logiką, jog jis grąžintų matavimo vienetus, pagal statinės savybės "heightUnits" reikšmę.
+//'7. Analogiškai pagal [4.]-[6.] punktus sukurkite savybę weight su statiniu išvardinimu "weightUnits", kurio pasirinkimai turi būti: "KG", "LBS"
+//'8. Sukurkite klasei Person metodą "toString". Kuris paverstų žmogaus savybes gražiu formatu: vardas ir pavardė pirmoje eilutėje, o "height" ir "weight" savybės atskirose eilutėse, atitrauktos nuo kairio krašto per "tab" simbolį, ir su matavimo vienetais(kurie išsaugoti) statinėse Person klasės savybėse'
+class Person {
+    constructor(name, surname, age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+    getFullname() {
+        return `${this.name} ${this.surname}`;
+    }
+    setAge(age) {
+        age > 1;
+        age <= 150;
+    }
+}
+const person = new Person('Deimantė', 'Rūtėnaitė', 25);
+console.log(person.getFullname());
+console.log(person.setAge(25));
